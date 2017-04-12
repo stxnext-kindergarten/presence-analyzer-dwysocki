e4363678283b5eb896f55dfcf0bf3fc0b816a398
Piotr Grundas <piotr.grundas@stxnext.pl>
2017-04-12 13:59:30 +0200

    Buildout.

--- src/presence_analyzer/main.py
+++ src/presence_analyzer/main.py
@@ -1,18 +1,7 @@
+
 # -*- coding: utf-8 -*-
-"""
-Flask app initialization.
-"""
-import os.path
+"""Flask app initialization."""
 from flask import Flask
 
 
-MAIN_DATA_CSV = os.path.join(
-    os.path.dirname(__file__), '..', '..', 'runtime', 'data', 'sample_data.csv'
-)
-
-
 app = Flask(__name__)  # pylint: disable=invalid-name
-app.config.update(
-    DEBUG=True,
-    DATA_CSV=MAIN_DATA_CSV
-)
